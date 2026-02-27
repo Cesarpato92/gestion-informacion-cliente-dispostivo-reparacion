bdde61e532483de6b16577c116b4c3694bab37c7
Cesarpato92 <parcerazo@gmail.com>
2026-02-27 00:58:58 -0500

    cambios en validaciones para cedula (solo numeros) y nombre (acepte ñ y tildes)

--- backend/src/models/cliente.model.js
+++ backend/src/models/cliente.model.js
@@ -11,14 +11,14 @@ export class Cliente {
             return false;
         }
         // Validar que la cedula solo contenga numeros y un maximo de 15 caracteres
-        if (!/^\d+/.test(this.cedula)) {
+        if (!/^\d+$/.test(this.cedula)) {
             return false;  // solo deben de ser numeros
         }
         if (this.cedula.length > 15) {
             return false;
         }
         // Validar que el nombre solo contenga letras y un maximo de 150 caracteres
-        if (!/^[a-zA-Z\s]+$/.test(this.nombre)) {
+        if (!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(this.nombre)) {
             return false;  // solo deben de ser letras y espacios
         }
         if (this.nombre.length > 150) {
@@ -41,6 +41,30 @@ export class Cliente {
             return false;
         }
         return true;
-}
+    }
+
+        toInsert() {
+            return {
+                cedula: this.cedula,
+                nombre: this.nombre,
+                email: this.email,
+                telefono: this.telefono
+            }
+        }
+        toUpdate() {
+            return {
+                nombre: this.nombre,
+                email: this.email,
+                telefono: this.telefono
+            }
+        }
+        // devuelve solo una fila de la base de datos, por lo que se le pasa un objeto con las propiedades de la tabla cliente
+        static fromDB(row) {
+            return new Cliente(row.cedula, row.nombre, row.email, row.telefono);
+        }
+        //retorna un array de objetos cliente a partir de un array de filas de la base de datos
+        static fromDBArray(rows) {
+            return rows.map(row => Cliente.fromDB(row));
+        }
 
 }
