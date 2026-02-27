4f80d3905dc380daf3f254a5c7fd9e4fad0b2757
Cesarpato92 <parcerazo@gmail.com>
2026-02-26 23:49:04 -0500

    creacion de modelo clienta con reclas de negocio para insercion a DB cumpliendo con las caracteristicas de los campos en la tabla clientes

---
+++ backend/src/models/Clientes.js
@@ -0,0 +1,46 @@
+export class Clientes {
+    constructor(cedula, nombre, email, telefono) {
+        this.cedula = cedula?.trim() || '';
+        this.nombre = nombre?.trim() || '';
+        this.email = email?.trim() || '';
+        this.telefono = telefono?.trim() || '';
+    }
+
+     validacion() {
+        if (!this.cedula || !this.nombre || !this.email || !this.telefono) {
+            return false;
+        }
+        // Validar que la cedula solo contenga numeros y un maximo de 15 caracteres
+        if (!/^\d+/.test(this.cedula)) {
+            return false;  // solo deben de ser numeros
+        }
+        if (this.cedula.length > 15) {
+            return false;
+        }
+        // Validar que el nombre solo contenga letras y un maximo de 150 caracteres
+        if (!/^[a-zA-Z\s]+$/.test(this.nombre)) {
+            return false;  // solo deben de ser letras y espacios
+        }
+        if (this.nombre.length > 150) {
+            return false;
+        }
+        // Validar que el telefono solo contenga numeros y un maximo de 15 caracteres
+        if (!/^\d+$/.test(this.telefono)) {
+            return false; 
+        }
+        if (this.telefono.length > 15) {
+            return false;
+        }
+        // Utilizacion de Regex para validar el formato del email, debe contener un @ y un . y un maximo de 100 caracteres
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(this.email)) {
+            return false;
+        }
+        
+        if (this.email.length > 100) {
+            return false;
+        }
+        return true;
+}
+
+}
