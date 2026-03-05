89d6f8fbbabd25eae03d0ee6b19f9e64e235c98c
Cesarpato92 <parcerazo@gmail.com>
2026-03-05 14:37:28 -0500

    creacion del server para el inicio del backend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,35 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import * as registroRutas from './src/routes/registro.routes.js'
+import validarJSON from './src/middleware/error.middleware.js'
+
+
+dotenv.config();
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+
+
+// Middleware
+app.use(express.json());
+app.use(validarJSON);
+app.use(cors());
+
+//Rutas
+app.use('/api', registroRutas)
+
+// error 404
+app.use((req, res) =>{
+    res.status(404).json({
+        success: false,
+        message: 'Ruta no encontrada'
+    })
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en http://localhost:${PORT}`)
+})
+
+export default app
