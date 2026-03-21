f0baac4b9e7fe0bb322c4fb88b041d85ad0b807a
Cesarpato92 <parcerazo@gmail.com>
2026-03-20 21:32:58 -0500

    Creacion navbar sin estilos, linkeado e instalacion de dependencias

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import {Link, useLocation} from 'react-router-dom'
+import './Navbar.css'
+import logo from '../img/Logo Pcelmedic.png'
 
 export const Navbar = () => {
+  const location = useLocation()
   return (
-    <div>Navbar</div>
+    <>
+    <nav className='navbar'>
+      <ul>
+        <li><img src={logo} alt="Logo Pcelmedic" className="logo" title='Dale vida a tu dispositivo!'/></li>
+                <li><Link className={location.pathname === '/' ? 'active' : ''} to="/">INICIO</Link></li>
+                <li><Link className={location.pathname === '/registro' ? 'active' : ''} to="/registro">REGISTRO</Link></li>
+                <li><Link className={location.pathname === '/reparaciones' ? 'active' : ''} to="/reparaciones">GESTION REPARACIONES</Link></li>
+                <li><Link className={location.pathname === '/garantias' ? 'active' : ''} to="/garantias">GARANTIAS</Link></li>
+                <li><Link className={location.pathname === '/facturacion' ? 'active' : ''} to="/facturacion">FACTURACION</Link></li>
+                <li><Link className={location.pathname === '/finanzas' ? 'active' : ''} to="/finanzas">FINANZAS</Link></li>
+      </ul>
+    </nav>
+    </>
   )
 }
