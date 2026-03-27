293dfe47f6567b7a5e3f237742d8d5d6e454abab
Cesarpato92 <parcerazo@gmail.com>
2026-03-26 23:29:16 -0500

    fix errores de layouds para el registro,

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,6 +1,14 @@
 
 export const Index = () => {
   return (
-    <div>index</div>
+    <div className="container">
+      <h1>Bienvenido a PCelMedic</h1>
+
+      <p>PCelMedic es una pagina de gestion de informacion para talleres de reparacion de celulares,
+        donde podras registrar clientes, dispositivos y reparaciones
+      </p>
+
+
+    </div>
   )
 }
